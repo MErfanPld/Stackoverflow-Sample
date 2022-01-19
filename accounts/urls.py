7c032d42d6b7ca03505e0b142603e0fad6fa01da
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2022-01-19 17:04:11 +0330

    Make login user and logout. create dashboard pase

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import UserRegister
+from .views import UserRegister, UserLogin, UserLogout, UserDashboard
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', UserRegister.as_view(), name="register"),
+    path('login/', UserLogin.as_view(), name='login'),
+    path('logout/', UserLogout.as_view(), name='logout'),
+    path('dashboard/<str:username>/', UserDashboard.as_view(), name='dashboard'),
 ]
