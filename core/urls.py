1a72b7834363a75d82b4f598589c71f124c74591
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2022-01-20 14:29:41 +0330

    Delete Bucket and run celery

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, BucketHome
+from .views import Home, BucketHome, BucketDelete
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('bucket/', BucketHome.as_view(), name="bucket"),
+    path('bucket-delete/<key>/', BucketDelete.as_view(), name="bucket-delete"),
 ]
