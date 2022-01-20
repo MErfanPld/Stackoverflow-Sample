7eeaaf8c27233e0fb2b865e7ae18dab977c7d813
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2022-01-20 13:48:07 +0330

    Show Bucket content in bucket.html

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, BucketHome
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('bucket/', BucketHome.as_view(), name="bucket"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .tasks import all_bucket_objects_task
 
 
 # Create your views here.
+
 
 class Home(View):
     def get(self, request):
@@ -10,3 +13,11 @@ class Home(View):
 
     def post(self, request):
         pass
+
+
+class BucketHome(LoginRequiredMixin, View):
+    template_name = 'core/bucket.html'
+
+    def get(self, request):
+        objects = all_bucket_objects_task()
+        return render(request, self.template_name, {'objects': objects})
