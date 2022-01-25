73b2ff67200e6c726e95012fcb633cdc782a0341
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2022-01-25 11:04:01 +0330

    Create Test models app core

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -13,6 +14,10 @@ class Question(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.title[:20]}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class Answer(models.Model):
